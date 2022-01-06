c1176e554e215e4a0c0e5e95d2a5904a827cd1ac
EvanAristy <evanjaristy@gmail.com>
2022-01-06 18:39:30 -0500

    folder structure

---
+++ dashboard/app.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>hello</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#container"));
